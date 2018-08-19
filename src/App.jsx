fb424326e2c15122e439f0f7b7edcf58f71c94b2
thitisarn <thitisarn_radomkij@outlook.com>
2018-08-19 14:56:45 +0700

    misc, debug/notes/lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import './stylesheets/App.css';
+import React, { Component } from 'react'
+import './stylesheets/App.css'
 
-import SearchQueryForm from './components/query';
-import DanbooruPost from './components/posts';
+import SearchQueryForm from './components/query'
+import DanbooruPost from './components/posts'
 
 // TODO: use lodash
 // TODO: use JedWatson/react-select, for a tag search
 // TODO: use Immutable.js
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#formatting-code-automatically
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-flow
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-a-css-preprocessor-sass-less-etc
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-custom-environment-variables
+// https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#developing-components-in-isolation
+
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {};
+    this.state = {}
     // there is no caching yet
 
     // non essential feature
@@ -29,8 +35,8 @@ class App extends Component {
         </header>
         <DanbooruPost />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './stylesheets/index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
+console.log('App started')
+console.log('process.env', process.env)
+
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
